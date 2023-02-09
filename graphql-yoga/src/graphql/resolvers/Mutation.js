a3adf25aa89357446910620da2fc232ca45c500c
dashfuentes <smis117112@gmail.com>
2023-02-08 18:49:45 -0600

    feat:wrapping the mutation up

--- graphql-yoga/src/graphql/resolvers/Mutation.js
+++ graphql-yoga/src/graphql/resolvers/Mutation.js
@@ -6,7 +6,18 @@ const Mutation = {
         const newNote = {title,content,date,author}
         const note = await Note.create( newNote );
         return note
+    },
+    async updateNote( _, { _id, title, content, date, author } ) {
+        const note = { title, content, date, author };
+        return await Note.findByIdAndUpdate(_id, note, {new : true} )
+    },
+
+    async removeNote( _, { _id } ) {
+        await Note.findByIdAndDelete( _id )
+        return await Note.find();
     }
+
+
 }
 
 export default Mutation
